2159316921b5cc25cf4dbce4ce0f07e4ccf59493
bagheera <r.boeran@gmail.com>
2025-04-14 15:00:51 +0700

    fix: swagger

--- src/index.js
+++ src/index.js
@@ -14,13 +14,15 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // Gunakan path absolut untuk file swagger
-const swaggerFilePath = path.join(__dirname, "doc", "swagger.yaml");
+// const swaggerFilePath = path.join(__dirname, "doc", "swagger.yaml");
 
 try {
-  const file = fs.readFileSync(swaggerFilePath, "utf8");
-  const swaggerDocument = YAML.parse(file);
+  const swaggerFilePath = path.join(__dirname, "doc", "swagger.yaml");
+  const swaggerFile = fs.readFileSync(swaggerFilePath, "utf8");
+  const swaggerDocument = YAML.parse(swaggerFile);
 
-  app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  // Swagger UI
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use("/auth", require("./auth/auth.controller"));
 
   app.listen(port, () => {
