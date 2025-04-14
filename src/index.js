d46eb043aa650842a3c3a330e50a7c716278a341
bagheera <r.boeran@gmail.com>
2025-04-14 14:57:12 +0700

    fix: swagger

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ const swaggerUi = require("swagger-ui-express");
 const fs = require("fs");
 const YAML = require("yaml");
 const path = require("path");
-const chokidar = require("chokidar");
 const express = require("express");
-
 const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
@@ -13,42 +12,22 @@ app.use(cors());
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
-const swaggerFilePath = path.join(__dirname, "doc/swagger.yaml");
 
-// Fungsi untuk memuat file Swagger YAML
-function loadSwaggerFile() {
-  try {
-    const fileContents = fs.readFileSync(swaggerFilePath, "utf8");
-    return YAML.parse(fileContents);
-  } catch (error) {
-    console.error("Error loading Swagger file:", error);
-    return null;
-  }
+// Gunakan path absolut untuk file swagger
+const swaggerFilePath = path.join(__dirname, "doc", "swagger.yaml");
+
+try {
+  const file = fs.readFileSync(swaggerFilePath, "utf8");
+  const swaggerDocument = YAML.parse(file);
+
+  app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use("/auth", require("./auth/auth.controller"));
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+} catch (error) {
+  console.error("Error loading Swagger file:", error);
+  console.log("Pastikan file doc/swagger.yaml ada di direktori proyek Anda");
+  process.exit(1); // Keluar dari proses jika file tidak ditemukan
 }
-
-let swaggerDocument = loadSwaggerFile();
-
-// Endpoint untuk Swagger UI
-app.use("/", swaggerUi.serve, (req, res, next) => {
-  // Setup Swagger UI dengan dokumen terbaru
-  swaggerUi.setup(swaggerDocument)(req, res, next);
-});
-
-// Pantau perubahan pada file YAML
-chokidar.watch(swaggerFilePath).on("all", (event, path) => {
-  if (event === "change") {
-    console.log("Swagger file changed, reloading...");
-    swaggerDocument = loadSwaggerFile();
-  }
-});
-
-// app.get("/", (req, res) => {
-//   res.send("Hello, untuk menggunakan api jika mencari produk maka harus /auth");
-// });
-
-// app.use("/user", require("./user/user.controller"))
-app.use("/auth", require("./auth/auth.controller"));
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
