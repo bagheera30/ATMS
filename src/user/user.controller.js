ca06fccc7e219a170e3752a8f3e333f4eb16a3b2
bagheera <r.boeran@gmail.com>
2025-04-30 19:23:28 +0700

    fix: user

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 const authMiddleware = require("../middlewares/autentication");
 const userService = require("./user.service");
 const router = express.Router();
-router.get("/", authMiddleware(["user"]), async (req, res) => {
+router.get("/", authMiddleware(["manager"]), async (req, res) => {
   try {
     const user = await userService.getUserall(req.user.username);
     res.status(200).json({
