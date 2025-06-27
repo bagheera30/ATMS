b035f192f4ae42097d4ca26a48dcc56bacc29c42
bagheera <r.boeran@gmail.com>
2025-06-27 22:01:07 +0700

    fix: download

--- src/atribut/atribut.controller.js
+++ src/atribut/atribut.controller.js
@@ -1,24 +1,29 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/autentication");
 const { getDownload } = require("./atribut.service");
 
 const router = express.Router();
 
-router.get("/:id/download", async (req, res) => {
-  const id = req.params.id;
-  console.log("Download request for ID:", id);
+router.get(
+  "/:id/download",
+  authMiddleware(["manager", "user"]),
+  async (req, res) => {
+    const id = req.params.id;
+    console.log("Download request for ID:", id);
 
-  try {
-    const { url } = await getDownload(id);
+    try {
+      const { url } = await getDownload(id);
 
-    // Redirect client ke MinIO dengan presigned URL
-    return res.redirect(url);
-  } catch (error) {
-    res.status(400).json({
-      code: 2,
-      status: false,
-      message: error.message,
-    });
+      // Redirect client ke MinIO dengan presigned URL
+      return res.redirect(url);
+    } catch (error) {
+      res.status(400).json({
+        code: 2,
+        status: false,
+        message: error.message,
+      });
+    }
   }
-});
+);
 
 module.exports = router;
